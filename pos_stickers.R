ef320b5c578afbd68eed5e77e4c58831ffd3a853
Mike Kearney <kearneymw@missouri.edu>
2018-04-16 17:45:22 -0500

    added background/about me

---
+++ pos_stickers.R
@@ -0,0 +1,10 @@
+x <- cc("50px 150px, 127px 285px, 204px 150px, 281px 285px, 204px 420px, 127px 15px")
+x <- unlist(strsplit(gsub("^\\s|px", "", x), "\\s"))
+x <- as.numeric(x)
+x <- tibble::as.tibble(matrix(x, ncol = 2, byrow = TRUE))
+names(x) <- c("x", "y")
+x$x <- x$x + 40
+x$y <- x$y + 35
+
+x <- apply(x, 1, function(x) paste(paste0(x, "px"), collapse = " "))
+pbcopy(paste(x, collapse = ", "))
